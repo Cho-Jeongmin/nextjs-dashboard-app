78db371f0bebd5b2ab10abea256563641c6ef3bc
Jeongmin Cho <jojm0829@gmail.com>
2025-11-14 16:12:14 +0900

    Chapter 4: Create Layouts and Pages

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,44 @@
+export default function Page() {
+  return (
+    <p className="border-2 border-solid border-red-500">
+      Invoices Page Invoices Page Invoices Page Invoices Page Invoices Page
+      Invoices Page Invoices Page Invoices Page Invoices Page Invoices Page
+      Invoices Page Invoices Page Invoices Page Invoices Page Invoices Page
+      Invoices Page Invoices Page Invoices Page Invoices Page Invoices Page
+      Invoices Page Invoices Page Invoices Page Invoices Page Invoices Page
+      Invoices Page Invoices Page Invoices Page Invoices Page Invoices Page
+      Invoices Page Invoices Page Invoices Page Invoices Page Invoices Page
+      Invoices Page Invoices Page Invoices Page Invoices Page Invoices Page
+      Invoices Page Invoices Page Invoices Page Invoices Page Invoices Page
+      Invoices Page Invoices Page Invoices Page Invoices Page Invoices Page
+      Invoices Page Invoices Page Invoices Page Invoices Page Invoices Page
+      Invoices Page Invoices Page Invoices Page Invoices Page Invoices Page
+      Invoices Page Invoices Page Invoices Page Invoices Page Invoices Page
+      Invoices Page Invoices Page Invoices Page Invoices Page Invoices Page
+      Invoices Page Invoices Page Invoices Page Invoices Page Invoices Page
+      Invoices Page Invoices Page Invoices Page Invoices Page Invoices Page
+      Invoices Page Invoices Page Invoices Page Invoices Page Invoices Page
+      Invoices Page Invoices Page Invoices Page Invoices Page Invoices Page
+      Invoices Page Invoices Page Invoices Page Invoices Page Invoices Page
+      Invoices Page Invoices Page Invoices Page Invoices Page Invoices Page
+      Invoices Page Invoices Page Invoices Page Invoices Page Invoices Page
+      Invoices Page Invoices Page Invoices Page Invoices Page Invoices Page
+      Invoices Page Invoices Page Invoices Page Invoices Page Invoices Page
+      Invoices Page Invoices Page Invoices Page Invoices Page Invoices Page
+      Invoices Page Invoices Page Invoices Page Invoices Page Invoices Page
+      Invoices Page Invoices Page Invoices Page Invoices Page Invoices Page
+      Invoices Page Invoices Page Invoices Page Invoices Page Invoices Page
+      Invoices Page Invoices Page Invoices Page Invoices Page Invoices Page
+      Invoices Page Invoices Page Invoices Page Invoices Page Invoices Page
+      Invoices Page Invoices Page Invoices Page Invoices Page Invoices Page
+      Invoices Page Invoices Page Invoices Page Invoices Page Invoices Page
+      Invoices Page Invoices Page Invoices Page Invoices Page Invoices Page
+      Invoices Page Invoices Page Invoices Page Invoices Page Invoices Page
+      Invoices Page Invoices Page Invoices Page Invoices Page Invoices Page
+      Invoices Page Invoices Page Invoices Page Invoices Page Invoices Page
+      Invoices Page Invoices Page Invoices Page Invoices Page Invoices Page
+      Invoices Page Invoices Page Invoices Page Invoices Page Invoices Page
+      Invoices Page Invoices Page Invoices Page Invoices Page
+    </p>
+  );
+}
